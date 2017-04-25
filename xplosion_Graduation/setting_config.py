0b74716c8cdce48368151aaa2f5d82b67efdc5b0
killa kam <mcclain.kamron@hotmail.com>
2017-04-25 02:25:30 -0500

    change img

--- xplosion_Graduation/setting_config.py
+++
@@ -1,7 +0,0 @@
-EMAIL_USE_TLS=True
-EMAIL_HOST='smtp.outlook.com'
-EMAIL_HOST_USER='xplosiondev@hotmail.com'
-EMAIL_HOST_PASSWORD='seniorDesign2016'
-EMAIL_HOST_PORT=587
-DEFAULT_FROM_EMAIL = 'xplosiondev@hotmail.com'
-SERVER_EMAIL = 'xplosiondev@hotmail.com'
